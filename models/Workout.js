0696712e98be244cf804caf1b24a87ffe160b9f7
tylerdahl123 <dahlgren15@gmail.com>
2021-01-15 09:46:27 -0800

    thanks bryan! i got it to work

--- models/Workout.js
+++ models/Workout.js
@@ -37,8 +37,18 @@ const WorkoutSchema = new Schema ({
           }
         }
     ]
+}, {
+    toJSON: {
+        virtuals: true
+    }
 });
 
+
+WorkoutSchema.virtual("totalDuration").get(function() {
+    return this.exercises.reduce((total, exercise) => {
+      return total + exercise.duration;
+    }, 0);
+  });
 
 const Workout = mongoose.model("Workout", WorkoutSchema );
 module.exports = Workout; 
