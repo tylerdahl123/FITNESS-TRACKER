6bc83b1b349cdce227d54b6b738683b0d216232b
tylerdahl123 <dahlgren15@gmail.com>
2021-01-14 17:09:29 -0800

    managed to add the put route but it doesnt work

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -7,7 +7,7 @@ const db = require("../models");
 
 
 module.exports = function (app){
-    app.get("./Workouts", function(req,res) {
+    app.get("/api/workouts", function(req,res) {
         db.Workout.find({})
         .then(dbWorkout => {
             res.json(dbWorkout);
@@ -15,5 +15,35 @@ module.exports = function (app){
         .catch(err => {
             res.json(err)
         })
-    })
+    });
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create({})
+        .then(data => res.json(data))
+        .catch(err=>{
+            res.json(err)
+        })
+    });
+    //got this code from the nosql activities. 
+    app.put("/api/workouts/:id", ({ params }, res) => {
+        db.Workout.updateOne(
+          {
+            _id: db.Workout(params.id)
+          },
+          {
+            $set: {
+              new: true
+            }
+          },
+      
+          (error, edited) => {
+            if (error) {
+              console.log(error);
+              res.send(error);
+            } else {
+              console.log(edited);
+              res.send(edited);
+            }
+          }
+        );
+      });
 }
